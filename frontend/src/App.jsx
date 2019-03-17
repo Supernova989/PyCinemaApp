335df4d23499049f93d4a32b95231c2066aaca4b
Max Isaev <ismak89@gmail.com>
2019-03-17 15:29:29 +0300

    updated package.json for Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
+import {Header} from './components/header';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="wrapper">
-				<header className="app-header">
-					test
-				</header>
+				<Header/>
 			</div>
 		);
 	}
