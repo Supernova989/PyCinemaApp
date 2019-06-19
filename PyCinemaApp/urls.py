856d1894b15f8f56da5b2c5d564c7ed09a73c834
Max Isaev <ismak89@gmail.com>
2019-06-20 00:22:20 +0300

    Basic serializer for Movies

--- PyCinemaApp/urls.py
+++ PyCinemaApp/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from frontend.views import IndexPage
 from django.conf import settings
 from django.conf.urls.static import static
+from movies.views import MovieView
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('movies', MovieView)
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('', IndexPage),
+	path('api/', include(router.urls)),
 	re_path(r'^(?:.*)/?$', IndexPage),  # Fallback
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
